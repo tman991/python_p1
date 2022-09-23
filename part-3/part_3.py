49d89e17777759ffc67aa09af9dbcdc3208bbf0e
Taha Manjoura <t.manjoura@gmail.com>
2022-09-23 14:59:50 -0500

    end_of_assignment

--- part-3/part_3.py
+++ part-3/part_3.py
@@ -6,18 +6,60 @@ my_book = {
     "pages": 374
 }
 
-# Follow the instructions in this part of the project. Define and flesh out your function below, which should accept a dictionary as an argument when called, and return a string of the info in that book-dictionary in a user-friendly readable format.
+# Follow the instructions in this part of the project. Define and flesh out your function below, 
+# which should accept a dictionary as an argument when called, and return a string of the info in 
+# that book-dictionary in a user-friendly readable format.
 
 # Code below
 
+def my_book(dict):
+
+print("my_book")
 
 
 
-# Once you are finished with that function, create several more functions which return individual pieces of information from the book.
+# Once you are finished with that function, create several more functions which return individual pieces of information 
+# from the book.
 
-# Code below
+my_title = {
+"title": "Then Hunger Games"
+}
+print(my_title(title)),
 
+my_author = {
+"author": "Suzanne Collins"
+}
+print(my_author("author"),
 
-# Finally, create at least three new functions that might be useful as we expand our home library app. Perhaps thing of a way you could accept additional arguments when the function is called? Also, imagine you have a list filled with dictionaries like above.
+my_rating = {
+"rating": 4.32
+}
+print(my_rating(rating))
 
-# Code below
+# Finally, create at least three new functions that might be useful as we expand our home library app. Perhaps thing of a way 
+# you could accept additional arguments when the function is called? Also, imagine you have a list filled with dictionaries like 
+# above.
+
+my_documentary = {
+    "title": "Dangerous Roads",
+    "author": "Al Collins",
+    "year": 2018,
+    "rating": 4.32,
+    "length": 60
+},
+
+my_movie = {
+    "title": "Vice",
+    "author": "Chris Brown",
+    "year": 2020,
+    "rating": 4.72,
+    "length": 120
+},
+
+my_magazine = {
+    "title": "People",
+    "author": "Oprah Wingield",
+    "year": 2022,
+    "rating": 4.82,
+    "length": 20
+}
--- part-4/part_4.py
+++ part-4/part_4.py
@@ -1,21 +1,37 @@
 ### Step 1 - Input function
 
-## Create five input statements to gather user's book they want to input to the system. After that be sure to turn it into a function.
+## Create five input statements to gather user's book they want to input to the system. After that be sure to turn it into a 
+# function.
 
-# Code here
+title = input('Enter book title')
+author = input('Enter autro name')
+year = input('Enter year made')
+rating = input('Enter book rating')
+pages = input('Enter book pages')
 
+my_book = {
+    "title": "Harry Potter",
+    "author": "J.K. Rowling",
+    "year": "2008",
+    "rating": "4.32",
+    "pages": "374"
+}
 
 ### Step 2 - Type conversion
 
-## Now convert the proper data-types front strings to either floats or ints depending on what it is. Feel free to comment out your old function so you don't get an error, or copy/paste and give it a new name.
+## Now convert the proper data-types front strings to either floats or ints depending on what it is. Feel free to comment out 
+# your old function so you don't get an error, or copy/paste and give it a new name.
 
-# Code here
+y = int(year)
+r = float(rating)
+p = int(pages)
 
 
 
 ### Step 3 - Error handling
 
-## Now take your previous function, and handle potential errors should the user type an answer that doesn't convert data-type properly.
+## Now take your previous function, and handle potential errors should the user type an answer that doesn't convert data-type 
+# properly.
 
 # Code here
 
@@ -25,12 +41,25 @@
 
 ## Now create a main menu function that gives the user options. Handle their choices with if/elif/else statements.
 
-# Code here
+def menu():
+    print("[1] Option 1")
+    print("[2] Option 2")
+    print("[0] Exit the program.")
+
+    menu()
+    option = int(input("Enter your option: "))
+
+while option != 0: 
+    if option == 1:
+        #do option 1 stuff
+        print("Option 1 has been called")
+    elif option == 2:
+        #do option 2 stuff
+        print("Option 2 has been called")
+    else: 
+        print("Invalid option.")
 
 
-### Step 5 - while loops
 
-## Now add a while loop to your main menu to keep it alive, and continually asking for input until the user chooses to exit the program. Call the main menu to ensure it functions properly.
 
-# Code here
 
